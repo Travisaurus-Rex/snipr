c6db706d077c3d59bc4bc5eca129c19d98a994e4
Travis Adams <brokenCD@live.com>
2025-09-24 09:49:18 +0700

    remove length param in code generator

--- internal/shortener/code.go
+++ internal/shortener/code.go
@@ -9,8 +9,8 @@ const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 var randSource = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-func GenerateCode(length int) string {
-	b := make([]byte, length)
+func GenerateCode() string {
+	b := make([]byte, 8)
 	for i := range b {
 		b[i] = charset[rand.Intn(len(charset))]
 	}
